d0182f7dd728efc1405b9ca663b480864093c2cd
garlickknight <garlickknight@gmail.com>
2025-11-17 12:28:58 -0700

    test

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,11 +2,11 @@ const weather = document.querySelector("#weather-icon");
 const temp = document.querySelector("#current-temp");
 const captionDesc = document.querySelector('figcaption');
 
-const myApi = "bff7a7671fa5d9a241cad669ec6b014b";
-const long = "49.75026562629286";
+const myApi = "1826e40be9fc1eaacee027527306cb27";
+const lon = "49.75026562629286";
 const lat = "6.635714416542802";
 
-const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myApi}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myApi}&units=metric`;
 
 async function apiFetch() {
     try {
@@ -14,7 +14,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data); // testing only
-            displayResults(data); // uncomment when ready
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
