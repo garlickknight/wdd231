0d5f11207b40546e2b48deff8183b1d3143c0229
garlickknight <garlickknight@gmail.com>
2025-11-18 17:02:09 -0700

    test changes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -5,3 +5,52 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navBar.classList.toggle("show");
 });
+
+const cardContainer = document.getElementById("cards");
+
+const display = (cardDisplay) => {
+    cardContainer.innerHTML = ""; // clear previous cards
+
+    cardDisplay.forEach(item => {
+        const section = document.createElement('section');
+        const name = document.createElement('h2');
+        const img = document.createElement('img');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const linkHolder = document.createElement('p');
+        const link = document.createElement("a");
+
+        name.textContent = item.name;
+        img.setAttribute("src", item.icon);
+        img.setAttribute("alt", `${item.name} icon`);
+        address.textContent = item.address;
+        phone.textContent = item.phone;
+        link.textContent = item.website;
+        link.setAttribute("href", item.website);
+
+        linkHolder.appendChild(link);
+
+        section.appendChild(name);
+        section.appendChild(img);
+        section.appendChild(address);
+        section.appendChild(phone);
+        section.appendChild(linkHolder);
+
+        cardContainer.appendChild(section);
+    });
+};
+
+async function getData() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+    console.table(data.members); // log members array for debugging
+    display(data.members);
+
+}
+
+getData();
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = document.lastModified;
